dde449936f62e911e78312c258040f3912f004bd
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-11-28 13:18:36 +0700

    fix(exercise6): add key-value pair - age = 18

--- ex-6.js
+++ ex-6.js
@@ -4,7 +4,9 @@ const student = {
   subjects: ["Math", "Science", "History"],
 };
 
+student.age = 18;
+
 console.log("Student Name: " + student.name);
-console.log("Student Age: " + student.grade);
+console.log("Student Age: " + student.age);
 console.log("Student Grade: " + student.grade);
 console.log("Student Subjects: " + student.subjects);
